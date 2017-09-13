47761056e5b8ee64f082bfa871e640102946e657
Digant Jagtap <digantjagtap@Guss-MacBook-Pro.local>
2017-09-13 01:18:26 -0400

    Added Login Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { CartPage } from '../pages/cart/cart';
+import { CloudPage } from '../pages/cloud/cloud';
+
+
 import { HomePage } from '../pages/home/home';
+
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  @ViewChild(Nav) navCtrl: Nav;
+    rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -18,5 +26,14 @@ export class MyApp {
       splashScreen.hide();
     });
   }
+  goToHome(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(HomePage);
+  }goToCart(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(CartPage);
+  }goToCloud(params){
+    if (!params) params = {};
+    this.navCtrl.setRoot(CloudPage);
+  }
 }
-
